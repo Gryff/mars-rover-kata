593c59877c180bc5d8450c6e938cfad34bd754af
Liam Griffin <gryffskin@gmail.com>
2017-03-29 14:16:50 +0100

    Refactor: all tests using testRover macro Not sure it is the most readable thing

--- javascript/spec/rover.spec.js
+++ javascript/spec/rover.spec.js
@@ -3,14 +3,9 @@
 import test from 'ava'
 import rover from '../src/rover'
 
-test('doesnt move when given no commands', t => {
-  t.is(rover.move(''), '00N')
-})
+test('doesnt move when given no commands', testRover, '', '00N')
 
-test('can move forward', t => {
-  t.is(rover.move('M'), '01N')
-})
-
+test('can move forward', testRover, 'M', '01N')
 test('can move forward twice', testRover, 'MM', '02N')
 
 function testRover (t, commands, expectedPosition) {
