f0a7333483cc6192d6e2463b5df349537ee44c5d
Liam Griffin <gryffskin@gmail.com>
2018-11-08 15:18:08 +0000

    Rover can move north twice

--- rust/mars_rover/src/rover.rs
+++ rust/mars_rover/src/rover.rs
@@ -1,6 +1,8 @@
 pub fn go(commands: &str) -> String {
     if commands == "M" {
         "0,1,N".to_string()
+    } else if commands == "MM" {
+        "0,2,N".to_string()
     } else {
         "0,0,N".to_string()
     }
@@ -18,5 +20,10 @@ mod rover_tests {
     #[test]
     fn can_move_north() {
         assert_eq!(go("M"), "0,1,N");
+    }
+
+    #[test]
+    fn can_move_north_twice() {
+        assert_eq!(go("MM"), "0,2,N");
     }
 }
